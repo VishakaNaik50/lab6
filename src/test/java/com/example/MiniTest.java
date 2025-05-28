f942fe30cfa6ed71d4d096a56d646ea0755e6c15
Vishaka Naik <vishakan42@gmail.com>
2025-05-28 21:28:55 +0530

    Update MiniTest.java

--- src/test/java/com/example/MiniTest.java
+++ src/test/java/com/example/MiniTest.java
@@ -1,3 +1,9 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class MiniTest {
     
     @Test
